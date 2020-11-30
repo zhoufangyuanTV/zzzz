0e0e8a47f676a8fa914cac5369382707444058ea
zhoufangyuanTV <zhoufangyuanPT@126.com>
2020-11-30 22:42:54 +0800

    2020-11-30AC 1problem

--- 2020-11-30/2020-11-30Normal/light/light.cpp
+++ 2020-11-30/2020-11-30Normal/light/light.cpp
@@ -18,43 +18,45 @@ inline void ins(int x,int y)
 	a[len].next=last[x];last[x]=len;
 }
 inline long long mymin(long long x,long long y){return x<y?x:y;}
-long long f[510000][2],g[510000];
+long long f[510000][3][2];
 char str[510000];
-bool b[510000];
-void bb(int x,int fa)
+int tot[510000];
+void dfs(int x,int fa)
 {
-	b[x]=str[x]=='1';
+	tot[x]=str[x]=='0';
 	for(int k=last[x];k>0;k=a[k].next)
 	{
 		int y=a[k].y;
 		if(y!=fa)
 		{
-			bb(y,x);
-			b[x]&=b[y];
+			dfs(y,x);
+			tot[x]+=tot[y];
 		}
 	}
 }
-int c[510000];
-void dfs(int x,int fa)
+long long ff[3][2],ss;
+void treedp(int x,int fa)
 {
-	c[x]=str[x]-'0';
-	c[x]^=1;g[x]=1;
-	f[x][1]=0;f[x][2]=0;
+	int c=str[x]-'0';
+	f[x][0][c^1]=f[x][1][c^1]=f[x][2][c^1]=1;
 	for(int k=last[x];k>0;k=a[k].next)
 	{
 		int y=a[k].y;
-		if(y!=fa&&!b[y])
+		if(y!=fa&&tot[y]>0)
 		{
-			dfs(y,x);
-			g[x]+=g[y]+1;
-			c[x]^=1;
-			if(c[y]==0)
-			{
-				g[x]++;
-				c[x]^=1;
-			}
+			treedp(y,x);
+			ff[0][0]=f[x][0][0];ff[1][1]=f[x][1][1];ff[2][0]=f[x][2][0];ff[0][1]=f[x][0][1];ff[1][0]=f[x][1][0];ff[2][1]=f[x][2][1];
+			f[x][0][0]=mymin(ff[0][1]+f[y][0][1]+1,ff[0][0]+f[y][0][0]+3);
+			f[x][0][1]=mymin(ff[0][0]+f[y][0][1]+1,ff[0][1]+f[y][0][0]+3);
+			f[x][1][0]=mymin(mymin(ff[1][1]+f[y][0][1]+1,ff[1][0]+f[y][0][0]+3),mymin(ff[0][0]+f[y][1][1],ff[0][1]+f[y][1][0]+2));
+			f[x][1][1]=mymin(mymin(ff[1][0]+f[y][0][1]+1,ff[1][1]+f[y][0][0]+3),mymin(ff[0][1]+f[y][1][1],ff[0][0]+f[y][1][0]+2));
+			f[x][2][0]=mymin(mymin(ff[2][1]+f[y][0][1]+1,ff[2][0]+f[y][0][0]+3),mymin(ff[1][0]+f[y][1][1],ff[1][1]+f[y][1][0]+2));
+			f[x][2][0]=mymin(f[x][2][0],mymin(ff[0][0]+f[y][2][0]+1,ff[0][1]+f[y][2][1]+3));
+			f[x][2][1]=mymin(mymin(ff[2][0]+f[y][0][1]+1,ff[2][1]+f[y][0][0]+3),mymin(ff[1][1]+f[y][1][1],ff[1][0]+f[y][1][0]+2));
+			f[x][2][1]=mymin(f[x][2][1],mymin(ff[0][1]+f[y][2][0]+1,ff[0][0]+f[y][2][1]+3));
 		}
 	}
+	if(tot[1]==tot[x])ss=mymin(ss,f[x][2][1]);
 }
 int main()
 {
@@ -64,8 +66,13 @@ int main()
 	len=0;memset(last,0,sizeof(last));
 	for(int i=1;i<n;i++)
 	{
-		int x,y;scanf("%d%d",&x,&y);
+		int x,y;read(x);read(y);
 		ins(x,y);ins(y,x);
 	}
+	dfs(1,0);
+	memset(f,63,sizeof(f));
+	ss=0x3f3f3f3f3f3f3f3fll;
+	treedp(1,0);
+	printf("%lld\n",ss);
 	return 0;
 }
