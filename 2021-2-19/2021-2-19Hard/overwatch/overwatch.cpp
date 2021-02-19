4bebde4c7f766e3fc576b9a28a9933ef47fb8a9b
zhoufangyuanTV <zhoufangyuanPT@126.com>
2021-02-20 07:54:16 +0800

    2021-2-19AC 1problem

--- 2021-2-19/2021-2-19Hard/overwatch/overwatch.cpp
+++ 2021-2-19/2021-2-19Hard/overwatch/overwatch.cpp
@@ -1,6 +1,7 @@
 #include<cstdio>
 #include<cstring>
 using namespace std;
+int Ka,c[110000];
 struct node
 {
 	int x,y,next;
@@ -10,168 +11,133 @@ inline void ins(int x,int y)
 	len++;a[len].x=x;a[len].y=y;
 	a[len].next=last[x];last[x]=len;
 }
-int K,c[110000],ss;
-void dfs(int x,int fa,int dep,int s)
+struct value{int val,p;};
+struct trnode
 {
-	if(dep==K)
+	int l,r,lc,rc;
+	value c,cc;
+}tr[1210000];int trlen;
+void bt(int l,int r)
+{
+	int now=++trlen;
+	tr[now].l=l;tr[now].r=r;tr[now].lc=tr[now].rc=-1;tr[now].c.val=tr[now].cc.val=0xc0c0c0c0;
+	if(l<r)
 	{
-		if(ss<s)ss=s;
-		return ;
-	}
-	for(int k=last[x];k>0;k=a[k].next)
-	{
-		int y=a[k].y;
-		if(y!=fa)dfs(y,x,dep+1,s+c[y]);
+		int mid=(l+r)/2;
+		tr[now].lc=trlen+1;bt(l,mid);
+		tr[now].rc=trlen+1;bt(mid+1,r);
 	}
 }
-int ff[110000],f[110000][7],g[110000];
+inline void update(int x)
+{
+	int lc=tr[x].lc,rc=tr[x].rc;
+	if(tr[lc].c.val>tr[rc].c.val)
+	{
+		tr[x].c=tr[lc].c;
+		if(tr[lc].cc.val>tr[rc].c.val)tr[x].cc=tr[lc].cc;
+		else tr[x].cc=tr[rc].c;
+	}
+	else
+	{
+		tr[x].c=tr[rc].c;
+		if(tr[lc].c.val>tr[rc].cc.val)tr[x].cc=tr[lc].c;
+		else tr[x].cc=tr[rc].cc;
+	}
+}
+void change(int now,int x,int c,int p)
+{
+	if(tr[now].l==tr[now].r){tr[now].c=(value){c,p};return ;}
+	int lc=tr[now].lc,rc=tr[now].rc;
+	int mid=(tr[now].l+tr[now].r)/2;
+	if(x<=mid)change(lc,x,c,p);
+	else change(rc,x,c,p);
+	update(now);
+}
+bool leaf[110000];
+int rk[110000],rt[110000][7];
+int fa[110000],f[110000][7],g[110000];
 inline int mymax(int x,int y){return x>y?x:y;}
 void treedp(int x)
 {
-	f[x][1]=c[x];f[x][2]=f[x][3]=f[x][4]=f[x][5]=0xc0c0c0c0;
-	g[x]=0xc0c0c0c0;
+	int cnt=0;
 	for(int k=last[x];k>0;k=a[k].next)
 	{
 		int y=a[k].y;
-		if(y!=ff[x])
+		if(y!=fa[x])rk[y]=++cnt;
+	}
+	if(cnt==0)
+	{
+		leaf[x]=true;
+		f[x][1]=c[x];f[x][2]=f[x][3]=f[x][4]=f[x][5]=f[x][6]=0xc0c0c0c0;
+		g[x]=f[x][Ka];
+		change(1,x,g[x],x);
+	}
+	for(int i=1;i<Ka;i++){rt[x][i]=trlen+1;bt(1,cnt);}
+	for(int k=last[x];k>0;k=a[k].next)
+	{
+		int y=a[k].y;
+		if(y!=fa[x])
 		{
-			ff[y]=x;
+			fa[y]=x;
 			treedp(y);
-			for(int i=1;i<5;i++)g[x]=mymax(g[x],f[y][i]+f[x][K-i]);
-			for(int i=1;i<5;i++)f[x][i+1]=mymax(f[x][i+1],f[y][i]+c[x]);
+			for(int i=1;i<Ka;i++)change(rt[x][i],rk[y],f[y][i],y);
 		}
 	}
-	g[x]=mymax(g[x],f[x][K]);
+	f[x][1]=c[x];
+	for(int i=2;i<=Ka;i++)f[x][i]=tr[rt[x][i-1]].c.val+c[x];
+	g[x]=f[x][Ka];
+	for(int i=1;i<=Ka-2;i++)
+	{
+		if(tr[rt[x][i]].c.p==tr[rt[x][Ka-i-1]].c.p)g[x]=mymax(g[x],tr[rt[x][i]].c.val+tr[rt[x][Ka-i-1]].cc.val+c[x]);
+		else g[x]=mymax(g[x],tr[rt[x][i]].c.val+tr[rt[x][Ka-i-1]].c.val+c[x]);
+	}
+	change(1,x,g[x],x);
 }
-int gg[110000];
 int main()
 {
 	freopen("overwatch.in","r",stdin);
 	freopen("overwatch.out","w",stdout);
-	int n,q;scanf("%d%d%d",&n,&q,&K);
+	int n,m;scanf("%d%d%d",&n,&m,&Ka);
 	for(int i=1;i<=n;i++)scanf("%d",&c[i]);
 	len=0;memset(last,0,sizeof(last));
-	memset(gg,0,sizeof(gg));
 	for(int i=1;i<n;i++)
 	{
 		int x,y;scanf("%d%d",&x,&y);
 		ins(x,y);ins(y,x);
-		gg[x]++;gg[y]++;
 	}
-	int gs=0;
-	for(int i=1;i<=n;i++)if(gg[i]==0)gs++;
-	if(gs<=100)
+	trlen=0;bt(1,n);
+	memset(leaf,false,sizeof(leaf));
+	fa[1]=0;treedp(1);
+	printf("%d\n",tr[1].c.val<-106110956?-1:tr[1].c.val);
+	while(m--)
 	{
-		ff[1]=0;treedp(1);
-		ss=0xc0c0c0c0;
-		for(int i=1;i<=n;i++)ss=mymax(ss,g[i]);
-		if(ss<-10611095)ss=-1;
-		printf("%d\n",ss);
-		for(int i=1;i<=q;i++)
+		int x,cc;scanf("%d%d",&x,&cc);
+		c[x]=cc;
+		f[x][1]=c[x];
+		if(!leaf[x])for(int i=2;i<=Ka;i++)f[x][i]=tr[rt[x][i-1]].c.val+c[x];
+		g[x]=f[x][Ka];
+		for(int i=1;i<=Ka-2;i++)
 		{
-			int x,w;scanf("%d%d",&x,&w);
-			c[x]=w;
-			f[x][1]=c[x];f[x][2]=f[x][3]=f[x][4]=f[x][5]=0xc0c0c0c0;
-			g[x]=0xc0c0c0c0;
-			for(int k=last[x];k>0;k=a[k].next)
-			{
-				int y=a[k].y;
-				if(y!=ff[x])
-				{
-					ff[y]=x;
-					treedp(y);
-					for(int i=1;i<5;i++)g[x]=mymax(g[x],f[y][i]+f[x][K-i]);
-					for(int i=1;i<5;i++)f[x][i+1]=mymax(f[x][i+1],f[y][i]+c[x]);
-				}
-			}
-			g[x]=mymax(g[x],f[x][k]);
-			if(x!=1)
-			{
-				x=ff[x];
-				f[x][1]=c[x];f[x][2]=f[x][3]=f[x][4]=f[x][5]=0xc0c0c0c0;
-				g[x]=0xc0c0c0c0;
-				for(int k=last[x];k>0;k=a[k].next)
-				{
-					int y=a[k].y;
-					if(y!=ff[x])
-					{
-						ff[y]=x;
-						treedp(y);
-						for(int i=1;i<5;i++)g[x]=mymax(g[x],f[y][i]+f[x][K-i]);
-						for(int i=1;i<5;i++)f[x][i+1]=mymax(f[x][i+1],f[y][i]+c[x]);
-					}
-				}
-				g[x]=mymax(g[x],f[x][k]);
-				if(x!=1)
-				{
-					x=ff[x];
-					f[x][1]=c[x];f[x][2]=f[x][3]=f[x][4]=f[x][5]=0xc0c0c0c0;
-					g[x]=0xc0c0c0c0;
-					for(int k=last[x];k>0;k=a[k].next)
-					{
-						int y=a[k].y;
-						if(y!=ff[x])
-						{
-							ff[y]=x;
-							treedp(y);
-							for(int i=1;i<5;i++)g[x]=mymax(g[x],f[y][i]+f[x][K-i]);
-							for(int i=1;i<5;i++)f[x][i+1]=mymax(f[x][i+1],f[y][i]+c[x]);
-						}
-					}
-					g[x]=mymax(g[x],f[x][k]);
-					if(x!=1)
-					{
-						x=ff[x];
-						f[x][1]=c[x];f[x][2]=f[x][3]=f[x][4]=f[x][5]=0xc0c0c0c0;
-						g[x]=0xc0c0c0c0;
-						for(int k=last[x];k>0;k=a[k].next)
-						{
-							int y=a[k].y;
-							if(y!=ff[x])
-							{
-								ff[y]=x;
-								treedp(y);
-								for(int i=1;i<5;i++)g[x]=mymax(g[x],f[y][i]+f[x][K-i]);
-								for(int i=1;i<5;i++)f[x][i+1]=mymax(f[x][i+1],f[y][i]+c[x]);
-							}
-						}
-						g[x]=mymax(g[x],f[x][k]);
-						if(x!=1)
-						{
-							x=ff[x];
-							f[x][1]=c[x];f[x][2]=f[x][3]=f[x][4]=f[x][5]=0xc0c0c0c0;
-							g[x]=0xc0c0c0c0;
-							for(int k=last[x];k>0;k=a[k].next)
-							{
-								int y=a[k].y;
-								if(y!=ff[x])
-								{
-									ff[y]=x;
-									treedp(y);
-									for(int i=1;i<5;i++)g[x]=mymax(g[x],f[y][i]+f[x][K-i]);
-									for(int i=1;i<5;i++)f[x][i+1]=mymax(f[x][i+1],f[y][i]+c[x]);
-								}
-							}
-							g[x]=mymax(g[x],f[x][K]);
-						}
-					}
-				}
-			}
-			ss=0xc0c0c0c0;
-			for(int i=1;i<=n;i++)ss=mymax(ss,g[i]);
-			if(ss<-10611095)ss=-1;
-			printf("%d\n",ss);
+			if(tr[rt[x][i]].c.p==tr[rt[x][Ka-i-1]].c.p)g[x]=mymax(g[x],tr[rt[x][i]].c.val+tr[rt[x][Ka-i-1]].cc.val+c[x]);
+			else g[x]=mymax(g[x],tr[rt[x][i]].c.val+tr[rt[x][Ka-i-1]].c.val+c[x]);
 		}
-	}
-	ss=0xc0c0c0c0;
-	for(int i=1;i<=n;i++)dfs(i,0,1,c[i]);
-	printf("%d\n",ss==0xc0c0c0c0?-1:ss);
-	for(int i=1;i<=q;i++)
-	{
-		int x,w;scanf("%d%d",&x,&w);c[x]=w;
-		ss=0xc0c0c0c0;
-		for(int i=1;i<=n;i++)dfs(i,0,1,c[i]);
-		printf("%d\n",ss==0xc0c0c0c0?-1:ss);
+		change(1,x,g[x],x);
+		int gg=1;
+		while(x!=1&&gg<Ka)
+		{
+			int y=x;x=fa[x];gg++;
+			for(int i=1;i<Ka;i++)change(rt[x][i],rk[y],f[y][i],y);
+			f[x][1]=c[x];
+			for(int i=2;i<=Ka;i++)f[x][i]=tr[rt[x][i-1]].c.val+c[x];
+			g[x]=f[x][Ka];
+			for(int i=1;i<=Ka-2;i++)
+			{
+				if(tr[rt[x][i]].c.p==tr[rt[x][Ka-i-1]].c.p)g[x]=mymax(g[x],tr[rt[x][i]].c.val+tr[rt[x][Ka-i-1]].cc.val+c[x]);
+				else g[x]=mymax(g[x],tr[rt[x][i]].c.val+tr[rt[x][Ka-i-1]].c.val+c[x]);
+			}
+			change(1,x,g[x],x);
+		}
+		printf("%d\n",tr[1].c.val<-106110956?-1:tr[1].c.val);
 	}
 	return 0;
 }
