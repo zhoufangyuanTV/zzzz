ff02c12f1cbfd4956608bf464a2cf88d4000ddb9
zhoufangyuanTV <zhoufangyuanPT@126.com>
2021-03-17 10:05:01 +0800

    2021-3-16AC 1problem

--- 2021-3-16/2021-3-16Hard/A/A.cpp
+++ 2021-3-16/2021-3-16Hard/A/A.cpp
@@ -1,46 +1,47 @@
 #include<cstdio>
 #include<cstring>
+#include<deque>
 #include<vector>
 using namespace std;
 vector<int> a;
+struct node{int l,r;};
+deque<node> b;
 int Ans[3010000];
 int main()
 {
 	freopen("A.in","r",stdin);
 	freopen("A.out","w",stdout);
 	int n,t;scanf("%d%d",&n,&t);
+	a.push_back(0);
 	for(int i=1;i<=n;i++)
 	{
 		char c=getchar();
 		while(c!='0'&&c!='1')c=getchar();
-		if(c=='1')a.push_back(i);
+		if(c=='0')a.push_back(0);
+		else (*a.rbegin())++;
 	}
-	bool bk=true;
-	for(int i=0;i<=a.size()-2;i++)if(a[i]+1==a[i+1]){bk=false;break;}
 	memset(Ans,0,sizeof(Ans));
-	if(bk)
+	for(int i=a.size()-1;i>=0;i--)
 	{
-		int las=0;
-		for(int i=0;i<a.size();i++)
+		node x;x.l=i-t;x.r=i-t+a[i]-1;
+		while(!b.empty()&&b.front().l<=x.r)
 		{
-			if(a[i]-t<=las)las++;
-			else las=a[i]-t;
-			Ans[las]=1;
+			x.r+=b.front().r-b.front().l+1;
+			b.pop_front();
+		}
+		if(x.l<=x.r)b.push_front(x);
+		if(!b.empty()&&b.back().r>=i)
+		{
+			Ans[i]=b.back().r-i+1;
+			b.back().r=i-1;
+			if(b.back().l>b.back().r)b.pop_back();
 		}
 	}
-	else
+	while(!b.empty()){Ans[0]+=b.back().r-b.back().l+1;b.pop_back();}
+	for(int i=0;i<a.size();i++)
 	{
-		while(t--)
-		{
-			for(int i=a.size()-1;i>=0;i--)
-			{
-				if(i==0){if(a[i]>1)a[i]--;}
-				else if(a[i]>a[i-1]+1)a[i]--;
-			}
-		}
-		for(int i=0;i<a.size();i++)Ans[a[i]]=1;
+		if(i>0)printf("0 ");
+		while(Ans[i]--)printf("1 ");
 	}
-	for(int i=1;i<n;i++){putchar(Ans[i]+'0');putchar(' ');}
-	putchar(Ans[n]+'0');putchar('\n');
 	return 0;
 }
